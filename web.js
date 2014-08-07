55da5b1546c80c7e38d618ddbbcb5a6565632064
meza <meza@meza.hu>
2014-08-07 14:19:28 +0100

    Added dummy implementation #1

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var fs = require('fs');
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res){
+	res.status(200).end()
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function () {
+	console.log("Listening on " + port);
+});
