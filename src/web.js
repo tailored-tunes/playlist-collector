04d734bc10c837a414ed29055c55f0c1f4a5fc8f
meza <meza@meza.hu>
2014-08-23 15:54:14 +0100

    Added cors

--- src/web.js
+++ src/web.js
@@ -1,19 +1,14 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/routes.js');
 
 var app = express();
-
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors());
 app.use(routes);
 
 var port = Number(process.env.PORT || 5000);
