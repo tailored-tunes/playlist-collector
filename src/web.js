f56466ecccf558ef22067ae0edb63e919d3550db
meza <meza@meza.hu>
2014-08-23 15:05:09 +0100

    Quote fix

--- src/web.js
+++ src/web.js
@@ -6,8 +6,8 @@ var routes = require('./routes/routes.js');
 var app = express();
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     next();
 });
 
