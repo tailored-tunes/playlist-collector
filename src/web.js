d2b8dd6120b723d4df1761ff37e6bb5b437979bc
meza <meza@meza.hu>
2014-08-23 15:01:31 +0100

    Enabled CORS

--- src/web.js
+++ src/web.js
@@ -5,6 +5,12 @@ var routes = require('./routes/routes.js');
 
 var app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
