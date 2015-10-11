a3be72c85f8071a537e07a0aa752dc45fb5e3686
meza <meza@meza.hu>
2015-10-11 23:49:27 +0100

    nsq things

--- src/lib/nsq.js
+++ src/lib/nsq.js
@@ -2,5 +2,6 @@ var nsq = require('nsqjs');
 
 var w = new nsq.Writer(process.env.NSQ_HOST, process.env.NSQ_PORT);
 
+var messageConverter = require('./messageConverter');
 var publisher = require('./nsq-publisher')(w, messageConverter);
 module.exports = publisher;
